a27cd57274b5397a0f91b298d9fd1b8b4c8824d4
farzadjamshidi <farzadjamshidicom@yahoo.com>
2024-01-28 19:49:32 +0000

    Add swagger to nest app

--- my-nest-app/src/main.ts
+++ my-nest-app/src/main.ts
@@ -5,17 +5,29 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+async function bootstrap()
+{
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('React Structure Sample APIs')
+    .setDescription('This is for training')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${ port }/${ globalPrefix }`
   );
 }
 
